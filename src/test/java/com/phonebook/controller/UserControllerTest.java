f8af4efc30aa9ed8636b7cff1b558c9bf6ba1765
Venjakosov <venja.kosov@gmail.com>
2016-11-07 21:58:21 +0200

    new tests
    create new tests methods for user model and main controller

--- src/test/java/com/phonebook/controller/UserControllerTest.java
+++ src/test/java/com/phonebook/controller/UserControllerTest.java
@@ -10,9 +10,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class UserControllerTest {
@@ -41,5 +43,36 @@ public class UserControllerTest {
         MockMvc mockMvc = standaloneSetup(controller).build();
 
         mockMvc.perform(post("/user/register")).andExpect(view().name("registerPage")).andExpect(model().hasErrors());
+    }
+
+    @Test
+    public void invalidUsernameValidation() throws Exception {
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(post("/user/register").param("username", "1aaaaa").param("firstName", "first")
+                .param("lastName", "last").param("middleName", "middle").param("password", "password")).andDo(print())
+                .andExpect(view().name("registerPage")).andExpect(model().hasErrors());
+    }
+
+    @Test
+    public void shouldProcessRegistration() throws Exception {
+        UserService service = mock(UserService.class);
+        when(service.register(unsaved)).thenReturn(saved);
+
+        UserController controller = new UserController(service);
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(post("/user/register")
+                .param("username", unsaved.getUsername()).param("firstName", unsaved.getFirstName())
+                .param("lastName", unsaved.getLastName()).param("middleName", unsaved.getMiddleName())
+                .param("password", unsaved.getPassword()))
+                .andExpect(redirectedUrl("/user/" + saved.getUsername() + "/contacts/all"));
+        verify(service, atLeastOnce()).register(unsaved);
+    }
+
+    @Test
+    public void shouldShowregisterPage() throws Exception {
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/user/register")).andExpect(view().name("registerPage"));
     }
 }
