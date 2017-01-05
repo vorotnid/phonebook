214324f9ed12987c2cce0b57005ac09526ee91b8
Alex <Alex.work911@ukr.net>
2017-01-05 17:40:01 +0200

    Code optimizing

--- src/test/java/com/phonebook/service/ContactServiceTest.java
+++ src/test/java/com/phonebook/service/ContactServiceTest.java
@@ -9,16 +9,15 @@ import java.util.List;
 
 public class ContactServiceTest extends InitServiceContextBase {
 
-
-    @Test(expected = PhoneNumberDuplicateException.class)
-    public void alreadyExistsTest() throws PhoneNumberDuplicateException {
-        contactService.createNewContact(new Contact("stubContact", "stubContact", "stubContact", "+380(55)5555555", "", "", ""), mockUser.getUsername());
-    }
-
     @Test
     public void addSuccessTest() throws PhoneNumberDuplicateException {
         Contact saved = contactService.createNewContact(new Contact("stubContact2", "stubContact2", "stubContact2", "+380(55)5555553", "", "", ""), mockUser.getUsername());
         Assert.assertEquals(mockContact2, saved);
+    }
+    
+    @Test(expected = PhoneNumberDuplicateException.class)
+    public void alreadyExistsTest() throws PhoneNumberDuplicateException {
+        contactService.createNewContact(new Contact("stubContact", "stubContact", "stubContact", "+380(55)5555555", "", "", ""), mockUser.getUsername());
     }
 
     @Test
@@ -28,16 +27,10 @@ public class ContactServiceTest extends InitServiceContextBase {
         Contact retrieved = contactService.updateContact(edited);
         Assert.assertEquals(edited, retrieved);
     }
+    
     @Test
     public void searchFirstNameTest() {
         List<Contact> found = contactService.findByRequestedParameters(mockContact1.getFirstName(), "", "", mockUser.getUsername());
-        Assert.assertNotNull(found);
-        Assert.assertTrue(found.size() == 1);
-    }
-
-    @Test
-    public void searchCellNumberNameTest() {
-        List<Contact> found = contactService.findByRequestedParameters("", "", mockContact1.getCellPhoneNumber(), mockUser.getUsername());
         Assert.assertNotNull(found);
         Assert.assertTrue(found.size() == 1);
     }
@@ -48,6 +41,13 @@ public class ContactServiceTest extends InitServiceContextBase {
         Assert.assertNotNull(found);
         Assert.assertTrue(found.size() == 1);
     }
+
+    @Test
+    public void searchCellNumberNameTest() {
+        List<Contact> found = contactService.findByRequestedParameters("", "", mockContact1.getCellPhoneNumber(), mockUser.getUsername());
+        Assert.assertNotNull(found);
+        Assert.assertTrue(found.size() == 1);
+    }    
 
     @Test
     public void searchAllNullTest() {
